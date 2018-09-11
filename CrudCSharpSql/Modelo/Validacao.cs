89c7200841de262460bc3e030b45ebd4850cefbf
marcosrduarte <marcosduarte.adsunip@gmail.com>
2018-09-11 09:14:11 -0300

    Criação da classe validação

--- CrudCSharpSql/Modelo/Validacao.cs
+++ CrudCSharpSql/Modelo/Validacao.cs
@@ -8,5 +8,28 @@ namespace CrudCSharpSql.Modelo
 {
     public class Validacao
     {
+        public String mensagem;
+        public int id;
+
+        public void ValidarDados(List<String> dadosPessoas)
+        {
+            if (dadosPessoas[1].Length > 30)
+                this.mensagem = "Nome com mais de 30 caracteres \n";
+            if (dadosPessoas[2].Length > 10)
+                this.mensagem += "Rg com mais de 10 caracteres \n";
+            if (dadosPessoas[3].Length < 13)
+                this.mensagem += "CPF com mais de 13 caracteres \n";
+
+            try
+            {
+                this.id = Convert.ToInt32(dadosPessoas[0]);
+            }
+            catch (FormatException e)
+            {
+
+                this.mensagem += "ID inválido";
+            }
+        }
+
     }
 }
